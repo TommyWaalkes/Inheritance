ca09b4e94f79f737eed58409c90794814d422fb0
TommyWaalkes <Tommy@grandcircus.co>
2017-10-04 20:29:56 -0400

    Initial Commit

---
+++ Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritence
+{
+    class Customer : Person
+    {
+        private string Customer_ID = "111222333";
+        private string payment_Info = "Cash";
+
+        public override void GetInfo()
+        {
+            // Calling the base class GetInfo method:
+            base.GetInfo();
+            Console.WriteLine("Customer ID: {0}", Customer_ID);
+            Console.WriteLine("Payment Method: {0}", payment_Info);
+        }
+
+        public override void PrintName()
+        {
+            Console.WriteLine(name + " Customer Id: " + Customer_ID);
+        }
+    }
+}
